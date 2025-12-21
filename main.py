dee9a5033ee35086fc95ecce51985192ad0c70e1
Artem <barovartem88@gmail.com>
2025-12-21 16:34:40 +0300

    added folder with png and text in programm

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import pygame
 
 pygame.init()
 
+
+num = 0
+
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
-click_png = pygame.image.load("click.png").convert()
 
+font = pygame.font.Font(None, 45)
+
+click_png = pygame.image.load("png\click.png").convert()
 pygame.display.set_caption("Clicker")
 pygame.display.set_icon(click_png)
 
+# цикл
 running = True
 while running:
 
@@ -17,6 +23,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    text_surface = font.render(f"Вы кликнули {num} раз", True, (0, 0, 0))
+    screen.blit(text_surface, (250, 25))
 
     clock.tick(60)
     pygame.display.update()
