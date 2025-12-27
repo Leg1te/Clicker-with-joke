d45e3fda3a6aecfe8493f78edf83becef390f392
Artem <barovartem88@gmail.com>
2025-12-27 10:06:22 +0300

    fix

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pygame
+import os
 
-pygame.mixer.pre_init(44100, -16, 1, 512)
+config_file = "configs/main_config.py"
+folder_scripts = "configs"
+
+# Создаём папку если её нет
+if not os.path.exists(folder_scripts):
+    os.makedirs(folder_scripts, exist_ok=True)
+
+# Создаём файл если его нет
+if not os.path.exists(config_file):
+    with open(config_file, "w") as f:
+        f.write("Score = 0")
 
 pygame.init()
 
+# микшер
+pygame.mixer.pre_init(44100, -16, 1, 512)
+
+# музыка
 sound_1 = pygame.mixer.music.load("sounds/1.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play()
 pygame.mixer.music.queue("sounds/3.mp3")
 
+# импорт
+import configs.main_config as cfg
 
-num = 0
 
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
@@ -18,12 +34,13 @@ clock = pygame.time.Clock()
 font = pygame.font.Font("font/Pixel.ttf", 23)
 font_1 = pygame.font.Font("font/Pixel.ttf", 13)
 
+# загрузка png
 click_png = pygame.image.load("png/click.png").convert()
+click_me = pygame.image.load("png/Click_me.png").convert_alpha()
+
 pygame.display.set_caption("Clicker")
 pygame.display.set_icon(click_png)
 
-# загрузка png
-click_me = pygame.image.load("png/Click_me.png").convert_alpha()
 
 # класс кнопок
 class button():
@@ -43,8 +60,9 @@ class button():
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
                 self.click = True
-                global num
-                num += 1
+                clikc_sound = pygame.mixer.Sound("sounds/Click.mp3")
+                clikc_sound.play()
+                cfg.Score += 1
 
             if pygame.mouse.get_pressed()[0] == 0:
                 self.click = False
@@ -73,13 +91,18 @@ while running:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                num += 1
+                clikc_sound = pygame.mixer.Sound("sounds/Click.mp3")
+                clikc_sound.play()
+                cfg.Score += 1
     
-    text_surface = font.render(f"Очков: {num}", True, (0, 0, 0))
+    text_surface = font.render(f"Очков: {cfg.Score}", True, (0, 0, 0))
     screen.blit(text_surface, (290, 30))
     
     text_surface_1 = font_1.render("P.s space - что-бы кликать", True, (0, 0, 0))
     screen.blit(text_surface_1, (220, 360))
+
+    text = font_1.render("Цель: 15000 очков", True, (0, 0, 0))
+    screen.blit(text, (270, 400))
 
     clock.tick(60)
     pygame.display.update()
